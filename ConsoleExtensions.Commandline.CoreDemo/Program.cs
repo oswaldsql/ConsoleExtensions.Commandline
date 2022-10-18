2f127c1ffe4ac6d62e885b12837f1161d7d04e8d
Lasse Sjørup <lsj@sweetgeeks.dk>
2022-10-18 16:10:50 +0200

    New demo

---
+++ ConsoleExtensions.Commandline.CoreDemo/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ConsoleExtensions.Commandline;
+using ConsoleExtensions.Commandline.CoreDemo;
+
+Controller.Run(new Toolbox(), args);
---
+++ ConsoleExtensions.Commandline.CoreDemo/Toolbox.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+
+namespace ConsoleExtensions.Commandline.CoreDemo
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    [Description("Tools for making life better.")]
+    internal class Toolbox
+    {
+        [Description("Sets your age to the specified value.")]
+        public Task<string> SetAgeAsync([Description("The requested age.")]int age)
+        {
+            if (age < 0)
+                throw new Exception("Setting your age to a negative value will break time and space as we know it");
+
+            Task.Delay(1000);
+
+            return Task.FromResult($"Your age is now {age}");
+        }
+    }
+}
